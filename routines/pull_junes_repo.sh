76eb259c9a1898441f803927b74f286e923eb791
Lance Zhang <lancezhang04@gmail.com>
2026-02-16 19:42:45 +0800

    Add bash scripts for automating routine tasks: pulling updates from Git repo and starting tmux session with Conda environment.

---
+++ routines/pull_junes_repo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ~/junes/routines
+git pull origin main
---
+++ routines/server_restart_junes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source /home/lancezhang04/miniconda3/etc/profile.d/conda.sh
+conda activate base
+
+/home/lancezhang04/miniconda3/bin/tmuxp load -d /home/lancezhang04/junes/routines/junes_tmux_session.yaml
