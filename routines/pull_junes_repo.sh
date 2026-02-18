8a7535f58eff45c73e00f741e2084c0ab4a98955
Lance Zhang <lancezhang04@gmail.com>
2026-02-18 11:52:54 +0800

    Automate Junes server restart in `pull_junes_repo.sh` after updating code.

--- routines/pull_junes_repo.sh
+++ routines/pull_junes_repo.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-cd ~/junes/routines
+cd /home/lancezhang04/junes
 git pull origin main
+
+# Restart Junes after code update
+tmux kill-session -t junes
+source /home/lancezhang04/junes/routines/server_restart_junes.sh
